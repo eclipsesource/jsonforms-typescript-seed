524c7fcd65af76d8ee04363a7dcf9192ecc1f190
Edgar Mueller <emueller@eclipsesource.com>
2016-07-28 10:30:43 +0200

    Change order of imports for custom renderer tutorial

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "jsonforms";
-import './app.ts';
-
 import "bootstrap/dist/css/bootstrap.css";
 import "angular-ui-bootstrap";
 import 'angular-ui-bootstrap/src/rating';
+
+import './app.ts';
